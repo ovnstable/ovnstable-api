4d3406b8b9d4024af99275b57849fb731c2c847e
y.pavlov <lenvonder12vorh@gmail.com>
2021-11-01 11:13:04 +0300

    Save to sheet

--- pushToSheet.js
+++ pushToSheet.js
@@ -4,13 +4,10 @@ const axios = require("axios");
 const _pushToSheet = (item) => {
 
     axios.post('https://sheet.best/api/sheets/09902320-b5ca-4a18-abac-7c7238002f57', item).then(value => {
-        console.log('Push to sheet response: ' + JSON.stringify(value.data))
     }).catch(reason => {
         console.log('Push to sheet error: ' + reason);
     });
 }
-
-
 
 module.exports = {
     pushToSheet: _pushToSheet
--- subscribe.js
+++ subscribe.js
@@ -7,6 +7,7 @@ let sheet = require('./pushToSheet.js');
 const fs = require('fs');
 
 let accounting = require("accounting-js")
+const e = require("express");
 let accountingConfig = {
     symbol: "",
     precision: 6,
@@ -15,6 +16,7 @@ let accountingConfig = {
 
 let Exchange = JSON.parse(fs.readFileSync('./contracts/Exchange.json'));
 let M2m = JSON.parse(fs.readFileSync('./contracts/Mark2Market.json'));
+let Ovn = JSON.parse(fs.readFileSync('./contracts/OvernightToken.json'));
 
 // let web3 = new Web3('https://polygon-mainnet.infura.io/v3/66f5eb50848f458cb0f0506cc1036fea');
 let web3 = new Web3('ws://localhost:8555');
@@ -25,6 +27,7 @@ web3.eth.net.getId().then(value => {
 
 let exchange = new web3.eth.Contract(Exchange.abi, Exchange.networks[137].address);
 let m2m = new web3.eth.Contract(M2m.abi, M2m.networks[137].address);
+let ovn = new web3.eth.Contract(Ovn.abi, Ovn.networks[137].address);
 
 const sequelize = new Sequelize('postgres://ovn_user:ovn_password@localhost:5432/ovn_analytics')
 
@@ -58,7 +61,7 @@ try {
 // a list for saving subscribed event instances
 const subscribedEvents = {}
 
-const subscribeLogEvent = (contract, eventName) => {
+const subscribeLogEvent = async (contract, eventName) => {
 
     console.log('Subscribe: ' + eventName)
     const eventJsonInterface = contract._jsonInterface.find((value, index) => value.name === eventName && value.type === 'event');
@@ -72,55 +75,128 @@ const subscribeLogEvent = (contract, eventName) => {
         if (!error) {
             const eventObj = web3.eth.abi.decodeLog(eventJsonInterface.inputs, result.data, result.topics.slice(1))
             console.log(`New ${eventName}!`, eventObj)
+            let date = new Date();
 
-            let data = m2m.methods.assetPricesForBalance().call();
+            if (eventName === 'EventExchange'){
+                let type = getType(eventObj);
 
-            data.then(value => {
-
-                let items = value.assetPrices;
-
-                let date = new Date();
-                for (let i = 0; i < items.length; i++) {
-
-                    let element = items[i];
-
-                    let symbol = element.symbol;
-                    let name = element.name;
-                    let bookValue = element.amountInVault / element.usdcPriceDenominator;
-                    let liquidationValue = element.usdcPriceInVault / element.usdcPriceDenominator;
-                    let price = element.usdcBuyPrice/ element.usdcPriceDenominator;
-                    let liquidationPrice = element.usdcSellPrice / element.usdcPriceDenominator;
-                    let bookPrice = element.usdcPriceInVault / element.usdcPriceDenominator ;
-
-                    if (!bookValue)
-                        bookValue = "0";
-
-                    if (!liquidationValue)
-                        liquidationValue = "0"
-
-                    if (!bookPrice)
-                        bookPrice = "0";
-
-                    let item = {
-                        createAt: date,
-                        symbol: symbol,
-                        name: name,
-                        bookValue: bookValue,
-                        liquidationValue: liquidationValue,
-                        price: price,
-                        liquidationPrice: liquidationPrice,
-                        bookPrice: bookPrice,
-                    };
-
-                    sheet.pushToSheet(item);
-                    // asset.create(newVar);
+                let res = {
+                    amount: eventObj[1] /10 ** 6,
+                    amountFee: eventObj[2] /10 ** 6,
+                    sender: eventObj[3]
                 }
 
-            })
+                saveActive(date, type, res)
+                saveOvn(date, type, res)
+            }else {
+
+                let res = {
+                    amount: "0",
+                    amountFee: "0",
+                    sender:"",
+                }
+
+                saveActive(date, 'Reward', res)
+                saveOvn(date, 'Reward', res)
+            }
+
         }
     })
     subscribedEvents[eventName] = subscription
 }
 
-subscribeLogEvent(exchange, 'EventExchange')
 
+function getType(eventObject){
+
+    let type = eventObject[0];
+
+    switch (type){
+        case 'buy':
+            return 'Mint';
+        case 'redeem':
+            return 'Redeem';
+    }
+
+}
+
+function saveOvn(date, type, res){
+
+    let totalOvn = ovn.methods.totalSupply().call();
+
+    totalOvn.then(value => {
+
+        let item = {
+            createAt: date,
+            active: 'ovn',
+            activeName: 'OvernightToken',
+            position: value / 10 ** 6,
+            marketPrice: "0",
+            netAssetValue: "0",
+            liquidationPrice: "0",
+            liquidationValue: "0",
+            type: type,
+            amount: res.amount,
+            amountFee: res.amountFee,
+            sender: res.sender
+
+        };
+        console.log(item)
+        sheet.pushToSheet(item);
+
+    })
+}
+
+
+function saveActive(date, type, res){
+    let data = m2m.methods.assetPricesForBalance().call();
+    data.then(value => {
+
+        let items = value.assetPrices;
+
+        for (let i = 0; i < items.length; i++) {
+
+            let element = items[i];
+
+            let symbol = element.symbol;
+            let name = element.name;
+            let bookValue = element.amountInVault / element.usdcPriceDenominator;
+            let liquidationValue = element.usdcPriceInVault / element.usdcPriceDenominator;
+            let price = element.usdcBuyPrice/ element.usdcPriceDenominator;
+            let liquidationPrice = element.usdcSellPrice / element.usdcPriceDenominator;
+            let bookPrice = element.usdcPriceInVault / element.usdcPriceDenominator ;
+
+            if (!bookValue)
+                bookValue = "0";
+
+            if (!liquidationValue)
+                liquidationValue = "0"
+
+            if (!bookPrice)
+                bookPrice = "0";
+
+            let item = {
+                createAt: date,
+                active: symbol,
+                activeName: name,
+                position: bookValue,
+                marketPrice: price,
+                netAssetValue: bookPrice,
+                liquidationPrice: liquidationPrice,
+                liquidationValue: liquidationValue,
+                type: type,
+                amount: res.amount,
+                amountFee: res.amountFee,
+                sender: res.sender
+            };
+
+            console.log(item)
+            sheet.pushToSheet(item);
+        }
+
+    });
+}
+
+
+
+subscribeLogEvent(exchange, 'EventExchange')
+subscribeLogEvent(exchange, 'RewardEvent');
