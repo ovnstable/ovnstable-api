a4f543280ae24a3e8ed9eb9b330ec0b591f317a7
y.pavlov <lenvonder12vorh@gmail.com>
2021-11-03 13:32:47 +0300

    Add log info

--- src/web3Service.js
+++ src/web3Service.js
@@ -6,21 +6,25 @@ let Exchange = JSON.parse(fs.readFileSync('./contracts/Exchange.json'));
 let M2m = JSON.parse(fs.readFileSync('./contracts/Mark2Market.json'));
 
 let web3URL = process.env.WEB3_URL
-
+console.log('Web3 URL ' + web3URL)
 let web3 = new Web3(web3URL);
 web3.eth.net.getId().then(value => {
     console.log('Network ID' + value)
 });
 
 
-let contract = new web3.eth.Contract(OverNightToken.abi, OverNightToken.networks[137].address);
-let M2mContract = new web3.eth.Contract(M2m.abi, M2m.networks[137].address);
+let ovn = new web3.eth.Contract(OverNightToken.abi, OverNightToken.networks[137].address);
+let m2m = new web3.eth.Contract(M2m.abi, M2m.networks[137].address);
 let exchange = new web3.eth.Contract(Exchange.abi, Exchange.networks[137].address);
+
+console.log('Exchange address: ' + exchange.options.address)
+console.log('M2m address: ' + m2m.options.address)
+console.log('Ovn address: ' + ovn.options.address)
 
 
 module.exports = {
-    ovn: contract,
-    m2m: M2mContract,
+    ovn: ovn,
+    m2m: m2m,
     exchange: exchange,
     exchangeJSON: Exchange,
     web3: web3
