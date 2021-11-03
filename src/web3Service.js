6a4c7b93dee1c5570ecedf3d978238836bdc2408
y.pavlov <lenvonder12vorh@gmail.com>
2021-11-03 13:50:45 +0300

    Add web3-providers-ws

--- src/web3Service.js
+++ src/web3Service.js
@@ -1,13 +1,38 @@
 const fs = require('fs');
 const Web3 = require('web3');
-
+const Web3WsProvider = require('web3-providers-ws');
 let OverNightToken = JSON.parse(fs.readFileSync('./contracts/OvernightToken.json'));
 let Exchange = JSON.parse(fs.readFileSync('./contracts/Exchange.json'));
 let M2m = JSON.parse(fs.readFileSync('./contracts/Mark2Market.json'));
 
+
+var options = {
+    timeout: 30000, // ms
+
+    // Useful for credentialed urls, e.g: ws://username:password@localhost:8546
+    headers: {
+        authorization: 'Basic username:password'
+    },
+
+    // Useful if requests result are large
+    clientConfig: {
+        maxReceivedFrameSize: 100000000,   // bytes - default: 1MiB
+        maxReceivedMessageSize: 100000000, // bytes - default: 8MiB
+    },
+
+    // Enable auto reconnection
+    reconnect: {
+        auto: true,
+        delay: 5000, // ms
+        maxAttempts: 5,
+        onTimeout: false
+    }
+};
+
+
 let web3URL = process.env.WEB3_URL
 console.log('Web3 URL ' + web3URL)
-let web3 = new Web3(web3URL);
+let web3 = new Web3(new Web3WsProvider(web3URL, options));
 web3.eth.net.getId().then(value => {
     console.log('Network ID' + value)
 });
