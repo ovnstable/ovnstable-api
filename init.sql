a046755fc6c69365a286748b88fc596d794d5ae1
y.pavlov <lenvonder12vorh@gmail.com>
2021-11-15 19:29:02 +0300

    update init.sql

--- init.sql
+++ init.sql
@@ -1,45 +1,9 @@
-create database ovn_analytics;
-create user ovn_user with encrypted password 'ovn_password';
-grant all privileges on database ovn_analytics to ovn_user;
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE SCHEMA IF NOT EXISTS anal;
 CREATE SCHEMA IF NOT EXISTS widget;
 
-
-GRANT USAGE ON SCHEMA public,anal, widget TO ovn_user;
-
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public,anal, widget TO ovn_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public,anal, widget TO ovn_user;
-GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public,anal, widget TO ovn_user;
-
-
-
-drop table if exists asset_prices_for_balance;
-create table asset_prices_for_balance
-(
-    id                uuid primary key,
-    active            varchar,
-    active_name       varchar,
-    created_at        timestamp,
-    updated_at        timestamp,
-
-    position          decimal,
-    market_price      decimal,
-    net_asset_value   decimal,
-    liquidation_price decimal,
-    liquidation_value decimal,
-    type              varchar,
-    amount            decimal,
-    amount_fee        decimal,
-    sender            varchar,
-    balance_ovn       decimal,
-    transaction_hash  varchar
-);
-
-grant all privileges on table asset_prices_for_balance to ovn_user;
 
 drop table if exists widget.distribution_rate;
 create table widget.distribution_rate
