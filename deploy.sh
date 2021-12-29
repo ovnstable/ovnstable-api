165554f3d80b5a463fd046b46e66361955bc9ca2
y.pavlov <lenvonder12vorh@gmail.com>
2021-12-29 21:06:32 +0300

    Add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,31 @@
 
 token=$1
 url=$2
+stand=$3
 tag=1
+
+
+if [ "$stand" = "prod" ]
+then
+  nameDapp="dapp-api"
+  deployments="polygon"
+  dockerComposePath="/root/ovn-dev/docker-compose.yaml"
+elif [ "$stand" = "dev" ]
+then
+  nameDapp="dapp-api-dev"
+  deployments="polygon_dev"
+  dockerComposePath="/root/ovn-dev/docker-compose.yaml"
+else
+  exit
+fi
+
+echo "$nameDapp"
+echo "$deployments"
 
 echo URL $url
 echo Token $token
 
-docker build . -t cr.yandex/crpg11k469bhc8lch9gm/overnight/dapp-api:$tag
+docker build . -t cr.yandex/crpg11k469bhc8lch9gm/overnight/$nameDapp:$tag
 
 
 docker login \
@@ -16,7 +35,7 @@ docker login \
          --password $token \
         cr.yandex
 
-docker push  cr.yandex/crpg11k469bhc8lch9gm/overnight/dapp-api:$tag
+docker push  cr.yandex/crpg11k469bhc8lch9gm/overnight/$nameDapp:$tag
 
 
 ssh $url docker login \
@@ -24,7 +43,7 @@ ssh $url docker login \
          --password $token \
         cr.yandex
 
-ssh $url docker pull cr.yandex/crpg11k469bhc8lch9gm/overnight/dapp-api:$tag
-ssh $url docker-compose -f /root/ovn/docker-compose.yaml up -d --no-deps dapp-api
+ssh $url docker pull cr.yandex/crpg11k469bhc8lch9gm/overnight/$nameDapp:$tag
+ssh $url docker-compose -f /root/ovn/docker-compose.yaml up -d --no-deps $nameDapp
 
-ssh $url docker logs ovn-dapp-api -f
+ssh $url docker logs ovn-$nameDapp -f
